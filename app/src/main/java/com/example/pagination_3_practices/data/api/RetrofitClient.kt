7f20e380386f374167da316ce7581fc5aa7c266e
someshsampat <someshsampat1234@gmail.com>
2026-05-10 17:12:42 +0530

    Remove okHttpClient configuration

--- app/src/main/java/com/example/pagination_3_practices/data/api/RetrofitClient.kt
+++ app/src/main/java/com/example/pagination_3_practices/data/api/RetrofitClient.kt
@@ -8,14 +8,8 @@ import java.util.concurrent.TimeUnit
 object RetrofitClient {
     private const val BASE_URL = "https://rickandmortyapi.com/api/"
 
-    private val okHttpClient = OkHttpClient.Builder()
-        .connectTimeout(30, TimeUnit.SECONDS)
-        .readTimeout(30, TimeUnit.SECONDS)
-        .build()
-
     val retrofit: CharacterApiService = Retrofit.Builder()
         .baseUrl(BASE_URL)
-        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
         .create(CharacterApiService::class.java)
